a478fb15fc9bb2fd8d464a6ac5eedcce793bf316
Neer12345988 <neer.saldanha@gmail.com>
2026-02-15 16:48:53 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import pgzrun
 
 WIDTH = 870
 HEIGHT = 650
-TITLE = "Who wants to be a Millionaire?"
+TITLE = "Speed Maths"
 
 marquee_box = Rect(0, 0, 880, 80)
 score_box = Rect(0, 0, 150, 50)
@@ -24,12 +24,12 @@ answer_box3.move_ip(20, 450)
 answer_box4.move_ip(370, 450)
 skip_box.move_ip(700, 270)
 
-money = 0
+score = 0
 question_file_name = "questions.txt"
-time_left = 10
+time = 10
 marquee_text = ""
 
-is_game_over = False
+game_over = False
 
 answer_boxes = [answer_box1, answer_box2, answer_box3, answer_box4]
 questions = []
@@ -41,27 +41,25 @@ def draw():
     global marquee_text
     
     screen.clear()
-    screen.fill("#003566")
-    screen.draw.filled_rect(marquee_box, "#003566")
-    screen.draw.filled_rect(score_box, "#44af69")
-    screen.draw.filled_rect(timer_box, "#44af69")
-    screen.draw.filled_rect(question_box, "#5c9ead")
+    screen.fill("#0EE0AF")
+    screen.draw.filled_rect(marquee_box, "#0EE0AF")
+    screen.draw.filled_rect(score_box, "#00ff00")
+    screen.draw.filled_rect(timer_box, "#00ff00")
+    screen.draw.filled_rect(question_box, "#0000FF")
     screen.draw.filled_rect(skip_box, "#F40A0A")
 
     for answer_box in answer_boxes:
         screen.draw.filled_rect(answer_box, "#0b6174")
 
-    marquee_text = f"Welcome to the Who wants to be a Millionaire game! You are on question {question_index} of {question_count}."
+    marquee_text = f"Welcome to the Speed Maths Test! You are on question {question_index} of {question_count}."
 
-    screen.draw.textbox(marquee_text, marquee_box, color = "#DA9B43")
-    screen.draw.textbox(f"Money: £{money}", score_box, color = "#F1C209")
-    screen.draw.textbox(str(time_left), timer_box, color = "white", shadow = (0.5, 0.5), scolor = "grey")
+    screen.draw.textbox(marquee_text, marquee_box, color = "#ff3700")
+    screen.draw.textbox(f"Score: {score}", score_box, color = "#F1F109")
+    screen.draw.textbox(str(time), timer_box, color = "white", shadow = (0.5, 0.5), scolor = "grey")
     screen.draw.textbox("Skip", skip_box, color = "#000000", angle = -90)
-    screen.draw.textbox("Hello World", question_box, color = "#fe901b", shadow = (0.3, 0.3), scolor = "#ff8706")
+    screen.draw.textbox("Hello World", question_box, color = "#1bfe2a", shadow = (0.3, 0.3), scolor = "#16f934")
     for answer_box in answer_boxes:
-        screen.draw.textbox("Hello World", answer_box, color = "#f7b267")
-
-    
+        screen.draw.textbox("Hello World", answer_box, color = "#f767ce")
 
 def update():
     move_marquee()
